528803219036ea873c97ecae3da813a11902af0e
Rangga Budi Pangestu <ranggabudipangestu@gmail.com>
2023-07-20 12:42:01 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const start= async() =>{
   const httpServer = await AppServer()
   await new Promise<void>((resolve) => httpServer.listen({ port: process.env.PORT || 4000}, resolve));
   console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+  console.log("TEST DISINI")
 }
 
 start()
